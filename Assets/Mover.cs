945493050be66e3f30bcd5cac970cf941987001c
Larisa <lara.carli@gmail.com>
2017-10-23 22:12:19 +0200

    Initial commit

---
+++ Assets/Mover.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour {
+
+	public float speed;
+
+	void Update() {
+		Instantiate (object, transform.position, transform.rotation);
+	}
+
+	void Start () {
+		Rigidbody rb = GetComponent<Rigidbody> ();
+		rb.velocity = transform.forward * speed;
+	}
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Boundary {
+	public float xMin, xMax, zMax, zMin;
+}
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public float tilt;
+	public Boundary boundary;
+
+	public GameObject shot;
+	public GameObject shotSpawn;
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Rigidbody rb = GetComponent<Rigidbody> ();
+
+		rb.velocity = new Vector3 (moveHorizontal, 0.0f, moveVertical) * speed;
+
+		rb.position = new Vector3(
+			Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
+		    0.0f,
+			Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax)
+		);
+
+		rb.rotation = Quaternion.Euler (0.0f, 0.0f, rb.velocity.x * -tilt);
+	}
+}
